f17856f187bf4a9686f8fa884739da7c5733bd19
chonommmmm <kdg.chono.mikiya@gmail.com>
2020-12-01 18:00:37 +0900

    dec 12.01 ground

---
+++ Assets/UnityScript/StartSceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneChange : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // change to GameMainScene
+        if(Input.GetKey(KeyCode.Space)){
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
