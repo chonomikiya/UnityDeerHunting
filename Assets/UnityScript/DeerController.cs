db16d3fa9a1bff175a1c4e26aa2e950604860882
chonommmm <kdg.chono.mikiya@gmail.com>
2020-12-16 16:54:33 +0900

    dec 12/16 fixed deer animation

--- Assets/UnityScript/DeerController.cs
+++ Assets/UnityScript/DeerController.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class DeerController : MonoBehaviour
 {
+    enum State {
+        walk,lookAround,idle,run
+    }
     Rigidbody m_rigidbody = null;
-    [SerializeField] float walkspeed = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody>();
-
     }
     void Update() {
-        if(Input.GetKeyDown(KeyCode.Return)){
-            AddWalk();
-        }
-        
+
     }
-    public void AddWalk(){
-        m_rigidbody.AddRelativeForce(Vector3.forward * walkspeed);
-    }
+
+
 
 }
